c5cab313f0049f63d3d5e1d743e5b575b5e0c73e
Diana Stancheva <diana@calderra.com>
2014-09-28 11:37:03 +0300

    uploading project

---
+++ MobileChatApp/MobileChatApp/scripts/app/settings.js
@@ -0,0 +1,11 @@
+/**
+ * Application Settings
+ */
+
+var appSettings = {
+
+    everlive: {
+        apiKey: '3Xg5eRJIktn0MNkV', // Backend Services API key 
+        scheme: 'http'
+    }
+}
---
+++ MobileChatApp/MobileChatApp/scripts/app/userProfile.js
@@ -0,0 +1,21 @@
+/**
+ * User profile view model
+ */
+var app = app || {};
+
+app.UserProfile = (function () {
+    'use strict';
+
+    var userProfileViewModel = (function () {
+
+
+
+        return {
+            title: "User Profile"
+        }
+
+    }());
+
+    return userProfileViewModel;
+
+}());
